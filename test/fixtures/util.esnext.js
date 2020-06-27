69a2bc2791958271ac368649bca64f22a39fdfc4
FND <fnd@localhost.localdomain>
2020-06-27 14:29:24 +0200

    added support for transpiling ESNext
    caveats:
    * much like with CommonJS globbing, `exclude` assumes `node_modules` is
      relative to the current working directory (as evidenced by awkward
      directory management in the corresponding test)
    * requires three dependencies (@rollup/plugin-babel, @babel/core and
      @babel/preset-env), so we'll probably need to introduce meta-packages
      to avoid burdening users with such internal details

---
+++ test/fixtures/util.esnext.js
@@ -0,0 +1,2 @@
+export let report = (...msg) => console.log(...msg); // eslint-disable-line no-console
+export let warn = (...msg) => console.error(...msg);
---
+++ test/test_transpiling.js
@@ -0,0 +1,79 @@
+/* global suite, test */
+"use strict";
+
+let { Bundle } = require("../lib/bundle");
+let { Config } = require("../lib/config");
+let { FIXTURES_DIR, fixturePath } = require("./util");
+let { strictEqual: assertSame } = require("assert");
+
+suite("transpiling ESNext");
+
+test("basic transpilation", async () => {
+	let bundle = makeBundle("./index.esnext.js");
+	let code = await bundle.compile();
+	/* eslint-disable max-len */
+	assertSame(code, `
+let warn = (...msg) => console.error(...msg);let lipsum = lang => \`[$\{lang}] lorem ipsum dolor sit amet\`;let echo = msg => warn(msg, lipsum("Latin"));export{echo};
+	`.trim());
+	/* eslint-enable max-len */
+
+	bundle = makeBundle("./index.esnext.js", { esnext: true });
+	code = await bundle.compile();
+	assertSame(code, `
+var warn = function warn() {
+  var _console2;
+
+  return (_console2 = console).error.apply(_console2, arguments);
+};var lipsum = function lipsum(lang) {
+  return "[".concat(lang, "] lorem ipsum dolor sit amet");
+};var echo = function echo(msg) {
+  return warn(msg, lipsum("Latin"));
+};export{echo};
+	`.trim());
+});
+
+test("Browserslist support", async () => {
+	let bundle = makeBundle("./index.esnext.js", {
+		esnext: {
+			browsers: ["current Node"]
+		}
+	});
+	let code = await bundle.compile();
+	/* eslint-disable max-len */
+	assertSame(code, `
+let warn = (...msg) => console.error(...msg);let lipsum = lang => \`[$\{lang}] lorem ipsum dolor sit amet\`;let echo = msg => warn(msg, lipsum("Latin"));export{echo};
+	`.trim());
+	/* eslint-enable max-len */
+});
+
+test("selectively skipping transpilation", async () => {
+	let cwd = process.cwd();
+	process.chdir(FIXTURES_DIR); // FIXME: smell
+	let bundle = makeBundle("./index.esnext.js", {
+		esnext: {
+			exclude: ["mylib"]
+		}
+	});
+	let code = await bundle.compile();
+	process.chdir(cwd);
+	/* eslint-disable max-len */
+	assertSame(code, `
+var warn = function warn() {
+  var _console2;
+
+  return (_console2 = console).error.apply(_console2, arguments);
+};let lipsum = lang => \`[$\{lang}] lorem ipsum dolor sit amet\`;var echo = function echo(msg) {
+  return warn(msg, lipsum("Latin"));
+};export{echo};
+	`.trim());
+	/* eslint-enable max-len */
+});
+
+function makeBundle(entryPoint, options) {
+	let config = new Config(fixturePath(entryPoint), {
+		compact: true,
+		resolve: true,
+		...options
+	});
+	return new Bundle(config);
+}
